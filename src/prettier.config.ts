047e41ffbf9f5ecd9f5cb601e00f63b0d0464fd0
Shayan Zamani <dev.shayan.zamani@gmail.com>
2025-07-27 17:45:42 +0330

    feat: provide types for the shared Prettier config

---
+++ src/prettier.config.ts
@@ -0,0 +1,45 @@
+import type { Config } from 'prettier';
+
+const prettierConfig: Config = {
+	semi: true,
+	useTabs: true,
+	tabWidth: 2,
+	vueIndentScriptAndStyle: false,
+	printWidth: 120,
+	singleQuote: true,
+	jsxSingleQuote: false,
+	quoteProps: 'consistent',
+	insertPragma: false,
+	requirePragma: false,
+	bracketSpacing: true,
+	bracketSameLine: false,
+	endOfLine: 'lf',
+	trailingComma: 'all',
+	arrowParens: 'always',
+	proseWrap: 'preserve',
+	objectWrap: 'preserve',
+	singleAttributePerLine: false,
+	htmlWhitespaceSensitivity: 'css',
+	embeddedLanguageFormatting: 'auto',
+	rangeStart: 0,
+	rangeEnd: Number.POSITIVE_INFINITY,
+	experimentalTernaries: false,
+	experimentalOperatorPosition: 'end',
+
+	overrides: [
+		{
+			files: ['*.jsonc', 'bun.lock'],
+			options: {
+				parser: 'json',
+			},
+		},
+		{
+			files: '*.svg',
+			options: {
+				parser: 'html',
+			},
+		},
+	],
+};
+
+export default prettierConfig;
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,21 +1,28 @@
 import { defineConfig } from 'tsdown/config';
 
 export default defineConfig({
-	unused: true,
-	publint: true,
-	unbundle: true,
 	format: ['esm', 'cjs'],
-	copy: ['./src/oxlint.config.jsonc', './prettier.config.js'],
-	dts: {
-		isolatedDeclarations: true,
-	},
+	copy: ['./src/oxlint.config.jsonc'],
+	entry: ['./src/index.ts', './src/prettier.config.ts'],
 	exports: {
 		customExports(packageExports) {
 			packageExports['./types/*'] = './dist/types/*';
 			packageExports['./oxlint'] = './dist/oxlint.config.jsonc';
-			packageExports['./prettier'] = './dist/prettier.config.js';
+			packageExports['./prettier'] = {
+				import: './dist/prettier.config.js',
+				require: './dist/prettier.config.cjs',
+			};
+			delete packageExports['./prettier.config'];
 
 			return packageExports;
 		},
 	},
+	dts: {
+		isolatedDeclarations: true,
+	},
+	unused: {
+		ignore: ['eslint-import-resolver-typescript'],
+	},
+	publint: true,
+	unbundle: true,
 });
