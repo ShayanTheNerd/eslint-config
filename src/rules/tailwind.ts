0283cbdf16b266f77d142ed9e5e321f2b67d222d
Shayan Zamani <dev.shayan.zamani@gmail.com>
2025-08-30 21:49:05 +0330

    refactor: remove unnecessarily disabled rules

--- src/rules/tailwind.ts
+++ src/rules/tailwind.ts
@@ -5,14 +5,7 @@ import type { DeepNonNullable } from '#types/helpers.d.ts';
 import { isEnabled } from '#utils/isEnabled.ts';
 import { defaultOptions } from '#utils/options/defaultOptions.ts';
 
-type VueRules = PluginRules<'vue'>;
-type CSSRules = PluginRules<'css'>;
 type StylisticRules = PluginRules<'@stylistic'>;
-type TailwindRules =
-	& PluginRules<'better-tailwindcss'>
-	& Pick<CSSRules, 'css/no-duplicate-imports'>
-	& Pick<StylisticRules, '@stylistic/max-len'>
-	& Pick<VueRules, 'vue/first-attribute-linebreak'>;
 
 function getTailwindRules(options: DeepNonNullable<Options>) {
 	const { tailwind, stylistic } = options.configs;
@@ -29,8 +22,6 @@ function getTailwindRules(options: DeepNonNullable<Options>) {
 
 	const tailwindRules = {
 		'@stylistic/max-len': 'off',
-		'css/no-duplicate-imports': 'off',
-		'vue/first-attribute-linebreak': 'off',
 
 		'better-tailwindcss/no-duplicate-classes': 'error',
 		'better-tailwindcss/no-deprecated-classes': 'error',
@@ -55,7 +46,7 @@ function getTailwindRules(options: DeepNonNullable<Options>) {
 				indent: useTabs ? 'tab' : indent,
 			},
 		],
-	} satisfies TailwindRules;
+	} satisfies PluginRules<'better-tailwindcss'> & Pick<StylisticRules, '@stylistic/max-len'>;
 
 	return tailwindRules;
 }
