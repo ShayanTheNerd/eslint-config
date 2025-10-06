5a3081136c3a6d897ef0c04ce9d0b143940f91c9
Shayan Zamani <dev.shayan.zamani@gmail.com>
2025-10-07 00:10:31 +0330

    fix(vue): sort attributes only based on the defined order

--- src/rules/vue.ts
+++ src/rules/vue.ts
@@ -87,11 +87,7 @@ function getVueRules(options: DeepNonNullable<Options>) {
 		}],
 
 		/* Priority C: Recommended (Potentially Dangerous Patterns) */
-		'vue/attributes-order': ['warn', {
-			alphabetical: true,
-			sortLineLength: true,
-			order: attributesOrder,
-		}],
+		'vue/attributes-order': ['warn', { order: attributesOrder }],
 
 		/* Miscellaneous */
 		'vue/camelcase': 'warn',
