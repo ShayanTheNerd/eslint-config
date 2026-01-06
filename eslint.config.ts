c463ac4318a6ad3c0cfea0c16da71df34af22262
Shayan Zamani <dev.shayan.zamani@gmail.com>
2026-01-06 12:04:14 +0330

    chore: remove unused ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from './src/index.ts';
 export default defineConfig(
   {
     autoDetectDeps: 'verbose',
-    configs: {
-      test: {
-        storybook: false,
-      },
-    },
   },
   {
     name: 'src/disables',
