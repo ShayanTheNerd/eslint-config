e9dc8a6877f36daaafe34908de2580b5d00caf44
Shayan Zamani <dev.shayan.zamani@gmail.com>
2026-06-04 22:15:03 +0330

    perf: leverage hoisting for static plugin rules

--- src/rules/promise.ts
+++ src/rules/promise.ts
@@ -1,24 +1,24 @@
 import type { PluginRules } from '#types/eslintRules.d.ts';
 
-function getPromiseRules() {
-  const promiseRules = {
-    'promise/always-return': ['error', { ignoreLastCallback: true }],
-    'promise/avoid-new': 'warn',
-    'promise/catch-or-return': ['error', { allowThenStrict: true, allowFinally: true }],
-    'promise/no-callback-in-promise': 'error',
-    'promise/no-multiple-resolved': 'error',
-    'promise/no-nesting': 'error',
-    'promise/no-new-statics': 'warn',
-    'promise/no-promise-in-callback': ['error', { exemptDeclarations: true }],
-    'promise/no-return-in-finally': 'error',
-    'promise/no-return-wrap': ['error', { allowReject: true }],
-    'promise/param-names': 'warn',
-    'promise/prefer-await-to-callbacks': 'warn',
-    'promise/prefer-await-to-then': ['warn', { strict: true }],
-    'promise/prefer-catch': 'warn',
-    'promise/valid-params': 'error',
-  } satisfies PluginRules<'promise'>;
+const promiseRules = {
+  'promise/always-return': ['error', { ignoreLastCallback: true }],
+  'promise/avoid-new': 'warn',
+  'promise/catch-or-return': ['error', { allowThenStrict: true, allowFinally: true }],
+  'promise/no-callback-in-promise': 'error',
+  'promise/no-multiple-resolved': 'error',
+  'promise/no-nesting': 'error',
+  'promise/no-new-statics': 'warn',
+  'promise/no-promise-in-callback': ['error', { exemptDeclarations: true }],
+  'promise/no-return-in-finally': 'error',
+  'promise/no-return-wrap': ['error', { allowReject: true }],
+  'promise/param-names': 'warn',
+  'promise/prefer-await-to-callbacks': 'warn',
+  'promise/prefer-await-to-then': ['warn', { strict: true }],
+  'promise/prefer-catch': 'warn',
+  'promise/valid-params': 'error',
+} satisfies PluginRules<'promise'>;
 
+function getPromiseRules() {
   return promiseRules;
 }
 
