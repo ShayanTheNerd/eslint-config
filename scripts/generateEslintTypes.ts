7ee7ecd6d0b185c5a588aad3c59bdc1ae291b968
Shayan Zamani <dev.shayan.zamani@gmail.com>
2026-07-27 15:13:02 +0330

    refactor(generateEslintTypes): simplify the script

--- scripts/generateEslintTypes.ts
+++ scripts/generateEslintTypes.ts
@@ -1,7 +1,5 @@
 import generateESLintTypes from 'eslint-typegen';
 import eslintPluginZodMini from 'eslint-plugin-zod-mini';
-import path from 'node:path';
-import fs from 'node:fs/promises';
 import { styleText } from 'node:util';
 
 import referenceConfig from './configs/eslint.config.reference.ts';
@@ -13,14 +11,11 @@ referenceConfig.push({
   },
 });
 
-const eslintSchemaPath = path.resolve('src/types/eslint-schema.d.ts');
+const eslintSchemaPath = 'src/types/eslint-schema.d.ts' as const;
 
-await fs.rm(eslintSchemaPath, { force: true });
 await generateESLintTypes(referenceConfig, {
   dtsPath: eslintSchemaPath,
   exportTypeName: 'ESLintSchema',
 });
 
-const successIcon = styleText('green', '✔');
-const styledESLintSchemaPath = styleText('blue', eslintSchemaPath);
-console.info(successIcon, `ESLint types generated at "${styledESLintSchemaPath}"`);
+console.info(styleText('green', '✔'), `ESLint types generated at "${styleText('blue', eslintSchemaPath)}"`);
